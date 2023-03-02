b7b98bbe5e17397370ba2c1e21e3dc5f1ad970af
emma <emma1.begard@gmail.com>
2023-03-02 08:55:53 +0100

    save result of the querry into the fichier.json file

--- Projet/script_python/recup_images.py
+++ Projet/script_python/recup_images.py
@@ -13,6 +13,11 @@ json_path=""
 json_data = json.load(open(json_path))
 data = json_normalize(json_data)
 
+
+
+
+
+"""
 # we get the link of the image and save it in a folder image
 link_imgs =data["image"]
 #we get the name of the cake
@@ -24,3 +29,8 @@ for i in range(len(link_imgs)):
     name = cake[i]# the name to save
     image = matplot.imread(link_imgs[i])# the image, a numpy table
     matplot.imsave(name, image)
+
+---------------------------------------------------------------------------------------------------------------------
+
+
+"""
---
+++ Projet/script_python/test.py
@@ -0,0 +1,6 @@
+import sys
+from SPARQLWrapper import SPARQLWrapper, JSON
+import json
+import crea_json as c
+
+c.crea_json()
