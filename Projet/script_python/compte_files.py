41a25e623bbcfa7b9f09b8251329c2b43f036b88
emma <emma1.begard@gmail.com>
2023-03-11 20:19:55 +0100

    push prediction pas foles

--- Projet/script_python/compte_files.py
+++
@@ -1,9 +0,0 @@
-import os, os.path
-
-def count_files(folder):
-    # simple version for working with CWD
-    return len([name for name in os.listdir(folder) if os.path.isfile(name)])
-
-    # path joining version for other paths
-    #DIR = '/tmp'
-    #return len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
